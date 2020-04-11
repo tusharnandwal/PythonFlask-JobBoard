d68516643e02d7346c51ca7801eb2e8601b6e957
tushar nandwal <tushar.nandwal@gmail.com>
2020-04-11 16:12:13 +0100

    commit for module 7

--- jobs/app.py
+++ jobs/app.py
@@ -1,7 +1,9 @@
 import sqlite3
 
 from flask import Flask
-from flask import  render_template,g
+from flask import render_template,g,request,redirect,url_for
+
+import datetime
 
 PATH="db/jobs.sqlite"
 
@@ -53,3 +55,16 @@ def employer(employer_id):
                         ,employer_id)
     return render_template("employer.html"
                             ,employer=employer,jobs=jobs,reviews=reviews)
+
+@app.route('/employer/<employer_id>/review', methods=['GET', 'POST'])
+def review(employer_id):
+    if request.method == 'POST':
+        review = request.form['review']
+        rating = request.form['rating']
+        title  = request.form['title']
+        status = request.form['status']
+        date = datetime.datetime.now().strftime("%m/%d/%Y")
+        execute_sql('INSERT INTO review (review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)'
+                    , (review, rating, title, date, status, employer_id), commit=True)
+        return redirect(url_for('employer',employer_id=employer_id))
+    return render_template("review.html",employer_id=employer_id)
